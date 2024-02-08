4d3bc15227bd95937f70d5170dc0d2119b3e5b3b
openpark <openpark.vip@outlook.com>
2024-02-08 08:48:54 +0800

    feature：基于 spring boot 的配置方式实现授权服务器，快速入门

--- authorization-server/authorization-server-quick-start/src/main/java/vip/openpark/authorization/AuthorizationServerQuickStartApplication.java
+++ authorization-server/authorization-server-quick-start/src/main/java/vip/openpark/authorization/AuthorizationServerQuickStartApplication.java
@@ -2,8 +2,13 @@ package vip.openpark.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 
 /**
+ * <div>
+ *     spring-boot-starter-oauth2-authorization-server
+ *     默认配置类 {@link OAuth2AuthorizationServerConfiguration} 为 OAuth2 授权服务器提供最小化配置
+ * </div>
  * @author anthony
  * @since 2024/2/8 0:05
  */
